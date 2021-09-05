7a89e76955e20896c05c16df551ab5836532bc3b
harsh malik <77454119+coderharshmalik@users.noreply.github.com>
2021-09-05 21:15:33 +0530

    Update septemberlong1.cpp

--- septemberlong1.cpp
+++ septemberlong1.cpp
@@ -1,1 +1,89 @@
+//Airline Restrictions
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d,e;
+        cin>>a>>b>>c>>d>>e;
+        if((a+b)<=d)
+        {
+            if(c<=e)
+            {
+                cout<<"YES"<<endl;
+            }
+            else if((b+c)<=d)
+            {
+                if(a<=e)
+                {
+                    cout<<"YES"<<endl;
+                }
+                else if((c+a)<=d)
+                {
+                    if(b<=e)
+                    {
+                        cout<<"YES"<<endl;
+                    }
+                    else
+                    cout<<"NO"<<endl;
+                }
+            }
+        }
+        //bvvuhhhhhhhhhhhh
+        //kjbkjcsjkkkkkkd
+        else if((b+c)<=d)
+        {
+            if(a<=e)
+            {
+                cout<<"YES"<<endl;
+            }
+            else if((c+a)<=d)
+            {
+                if(b<=e)
+                {
+                    cout<<"YES"<<endl;
+                }
+                else if((a+b)<=d)
+                {
+                    if(c<=e)
+                    {
+                        cout<<"YES"<<endl;
+                    }
+                    else
+                    cout<<"NO"<<endl;
+                }
+            }
+        }
+        //dbuyucycds
+        //kdcibuiwdbi
+        else if((c+a)<=d)
+        {
+            if(b<=e)
+            {
+                cout<<"YES"<<endl;
+            }
+            else if((a+b)<=d)
+            {
+                if(c<=e)
+                {
+                    cout<<"YES"<<endl;
+                }
+                else if((b+c)<=d)
+                {
+                    if(a<=e)
+                    {
+                        cout<<"YES"<<endl;
+                    }
+                    else
+                    cout<<"NO"<<endl;
+                }
+            }
+        }
+
+    }
+    return 0;
+}
